7bcbe03407ca269a399e8fc7548326d7270e89b5
alcemirjunior <alcemirmjunior@gmail.com>
2020-11-28 01:36:16 -0300

    primeiro commit

---
+++ src/test/java/jr/alcemir/personAPI/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jr.alcemir.personAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
