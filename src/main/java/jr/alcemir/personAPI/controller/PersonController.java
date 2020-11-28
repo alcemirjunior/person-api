08c6ef6e91e6fdf97bc7b0276e0476a0f81ba8ce
alcemirjunior <alcemirmjunior@gmail.com>
2020-11-28 02:39:19 -0300

    adicionada a primeira operacao em PersonController

--- src/main/java/jr/alcemir/personAPI/controller/PersonController.java
+++ src/main/java/jr/alcemir/personAPI/controller/PersonController.java
@@ -1,17 +1,30 @@
 package jr.alcemir.personAPI.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jr.alcemir.personAPI.dto.MessageResponseDTO;
+import jr.alcemir.personAPI.entity.Person;
+import jr.alcemir.personAPI.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API Test!";
+    private PersonRepository personRepository;
+    //aqui entra a tal da injeção de dependência!!!
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
 
+    @PostMapping //e não GetMapping pois agora queremos criar um livro
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
 }
---
+++ src/main/java/jr/alcemir/personAPI/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package jr.alcemir.personAPI.repository;
+
+import jr.alcemir.personAPI.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository <Person, Long>{
+
+}
